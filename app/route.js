6781ff594efd18e3b17cef9fa091dc57e17ab4d1
Bigless27 <lessonryan@gmail.com>
2016-03-15 19:52:32 -0500

    Comment in next steps for further development

--- app/route.js
+++ app/route.js
@@ -1,14 +1,14 @@
 var nameApp = angular.module('nameApp',['ngRoute']);
-
+//fix routing and include file
 nameApp.config(function($routeProvider){
-  $routeProvider.
+  $routeProvider
     .when('/',{
       controller: "MainController",
       templateUrl: 'index.html'
     })
     .when('/:taskName',
     {
-      controller: 'MainController',
+      controller: 'SimpleController',
       templateUrl: "views/View1.html"
     })
     .otherwise({redirectTo: '/'});
